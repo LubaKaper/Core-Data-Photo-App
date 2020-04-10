2cab5d5f31ef5ae1cb052108e561f4d48b74ce82
LubaKaper <liubovkaper@pursuit.org>
2020-04-10 12:27:33 -0400

    Initial Commit

---
+++ Core-Data-Photo-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core-Data-Photo-App
+//
+//  Created by Liubov Kaper  on 4/10/20.
+//  Copyright © 2020 Luba Kaper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
